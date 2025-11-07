e574e10e4d0dc60ad1ae81619ba6d90b20d157db
Marta Ladrón de Guevara Cano <mzladron72@gmail.com>
2025-11-07 13:18:33 +0100

    cambio de proyecto java tipo maven a tipo ant

---
+++ Seminario1_DDSI/src/seminario1_ddsi/Seminario1_DDSI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package seminario1_ddsi;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Seminario1_DDSI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
